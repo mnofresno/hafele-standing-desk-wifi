1994a590e6076a65755dc31912d662a7f0414b1d
mariano.fresno <mnofresno@gmail.com>
2022-08-01 20:50:07 -0300

    Add motor driver and non-blocking time control

--- firmware/MotorDriver.cpp
+++ firmware/MotorDriver.cpp
@@ -8,35 +8,79 @@ MotorDriver::MotorDriver(
     _motor_down_pin = motor_down_pin;
 }
 
-void MotorDriver::process() {
+void MotorDriver::run() {
+    if (
+        _current_move_state != _expected_state
+    ) {
+        switch (_expected_state) {
+            case MOVE_STATE_UP:
+                doMoveUp();
+                _current_move_state = MOVE_STATE_UP;
+                break;
+            case MOVE_STATE_DOWN:
+                doMoveDown();
+                _current_move_state = MOVE_STATE_DOWN;
+                break;
+            case MOVE_STATE_STOP:
+                doStop();
+                _current_move_state = MOVE_STATE_STOP;
+                break;
+        }
+    }
+
+    if (_current_move_state != MOVE_STATE_STOP && timedOut() && _duration != 0) {
+        _expected_state = MOVE_STATE_STOP;
+        _duration = 0;
+    }
 }
 
-void MotorDriver::moveUpForMillis(int duration) {
-    moveUp();
-    delay(duration);
-    moveStop();
+bool MotorDriver::timedOut() {
+    return millis() - _started_movement_at >= _duration;
 }
 
-void MotorDriver::moveDownForMillis(int duration) {
-    moveDown();
-    delay(duration);
-    moveStop();
+void MotorDriver::moveForMillis(int direction, unsigned long duration) {
+    _started_movement_at = millis();
+    _expected_state = direction;
+    _duration = duration;
 }
 
-void MotorDriver::moveUp() {
+void MotorDriver::moveUpForMillis(unsigned long duration) {
+    moveForMillis(MOVE_STATE_UP, duration);
+}
+
+void MotorDriver::moveDownForMillis(unsigned long duration) {
+    moveForMillis(MOVE_STATE_DOWN, duration);
+}
+
+void MotorDriver::doMoveUp() {
     // enable_wdt();
     digitalWrite(_motor_up_pin, HIGH);
     digitalWrite(_motor_down_pin, LOW);
 }
 
-void MotorDriver::moveDown() {
+void MotorDriver::doMoveDown() {
     // enable_wdt();
     digitalWrite(_motor_down_pin, HIGH);
     digitalWrite(_motor_up_pin, LOW);
 }
 
-void MotorDriver::moveStop() {
+void MotorDriver::doStop() {
     digitalWrite(_motor_up_pin, LOW);
     digitalWrite(_motor_down_pin, LOW);
     // disable_wdt();
+}
+
+void MotorDriver::moveUp() {
+    _expected_state = MOVE_STATE_UP;
+    _duration = 0;
+}
+
+void MotorDriver::moveDown() {
+    _expected_state = MOVE_STATE_DOWN;
+    _duration = 0;
+}
+
+void MotorDriver::stop() {
+    _expected_state = MOVE_STATE_STOP;
+    _duration = 0;
 }
