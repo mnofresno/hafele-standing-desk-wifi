de2499190595c8a5e9b09d899120c1caff44938b
mariano.fresno <mnofresno@gmail.com>
2022-08-06 01:24:31 -0300

    Fix calibration service * Add display set cursor to display handler * Refactor move all ifss for menu translation into switch-case

--- firmware/Calibration.cpp
+++ firmware/Calibration.cpp
@@ -1,36 +1,47 @@
 #include "Calibration.h"
-#include <EEPROM.h>
-
+#include <ArduinoJson.h>
 
 Calibration::Calibration(std::function<void()> onCorruptedEepromCallback) {
     _onCorruptedEepromCallback = onCorruptedEepromCallback;
 }
 
-CalibrationData Calibration::fetch() {
-    read_eeprom();
-    if (_data.salt != EEPROM_SALT) {
-        Serial.println("Invalid settings in EEPROM, trying with defaults");
+void Calibration::fetch(CalibrationData &data) {
+    _preferences.begin("StandingDesk", false);
+
+    String serializedSettings = _preferences.getString("settings", "{\"current_position_mm\":-1}");
+    DynamicJsonDocument parsedJson(1024);
+
+    deserializeJson(parsedJson, serializedSettings);
+
+    data.current_position_mm = parsedJson["current_position_mm"];
+    if (data.current_position_mm == -1) {
+        data = {};
         _onCorruptedEepromCallback();
-        CalibrationData defaults;
-        _data = defaults;
     }
-    return _data;
+    data.down_traverse_mm_sec = parsedJson["down_traverse_mm_sec"];
+    data.up_traverse_mm_sec = parsedJson["up_traverse_mm_sec"];
+    data.is_dirty = false;
+
+
+    _preferences.end();
 }
 
-void Calibration::store(CalibrationData data) {
-    _data = data;
-    save_eeprom();
-}
+void Calibration::store(CalibrationData &data) {
+    _preferences.begin("StandingDesk", false);
 
-void Calibration::read_eeprom() {
-    EEPROM.begin(512);
-    EEPROM.get(0, _data);
-    EEPROM.end();
-}
+    DynamicJsonDocument parsedJson(1024);
 
-void Calibration::save_eeprom() {
-    EEPROM.begin(512);
-    EEPROM.put(0, _data);
-    EEPROM.commit();
-    EEPROM.end();
+    parsedJson["current_position_mm"] = data.current_position_mm;
+    parsedJson["down_traverse_mm_sec"] = data.down_traverse_mm_sec;
+    parsedJson["up_traverse_mm_sec"] = data.up_traverse_mm_sec;
+
+    String serializedJson;
+
+    serializeJson(parsedJson, serializedJson);
+
+    _preferences.putString("settings", serializedJson);
+
+    data.is_dirty = false;
+
+    _preferences.end();
 }
