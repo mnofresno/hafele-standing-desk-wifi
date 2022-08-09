d910b3c1a364db90ac2cdb616435f97997b0bbd0
mariano.fresno <mnofresno@gmail.com>
2022-08-09 01:34:58 -0300

    Add state change debounce

--- firmware/ButtonsHandler.h
+++ firmware/ButtonsHandler.h
@@ -9,7 +9,11 @@ class ButtonsHandler {
         uint8_t _back_button_pin;
         uint8_t _invert_enter_button;
         uint8_t _invert_back_button;
+        unsigned long _last_state_change;
+        bool _state_has_changed_recently;
         int expected_level_for(bool inverted_button);
+        bool do_read_button(uint8_t pin_number, bool invert);
+        bool debounce_state_change(bool button_reading);
     public:
         ButtonsHandler(
             uint8_t enter_button_pin,
@@ -19,6 +23,7 @@ class ButtonsHandler {
         );
         bool readEnterButton();
         bool readBackButton();
+        void process(bool has_changed_state);
         String serialize_buttons_debug();
 };
 #endif
